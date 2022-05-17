638ccaf497d5dcbed02941496000955f8e52e12b
restuarachman <restuarachman@gmail.com>
2022-05-18 01:18:15 +0700

    edit user test

--- controller/user_test.go
+++ controller/user_test.go
@@ -3,15 +3,17 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"ticketing/driver/mock"
-	"ticketing/helper/encrypt"
+	"ticketing/middleware/constants"
 	"ticketing/model/domain"
 	"ticketing/model/response"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type GetAllResponse struct {
@@ -42,8 +44,7 @@ func TestUserControllerLogin(t *testing.T) {
 	e := echo.New()
 
 	us := mock.NewMockUserService()
-	pw, _ := encrypt.Hash("rahasia")
-	us.Save(domain.User{Username: "dono", Password: pw})
+	us.Save(domain.User{Username: "dono", Password: "rahasia"})
 
 	user := domain.User{Username: "dono", Password: "rahasia"}
 
@@ -150,36 +151,61 @@ func TestUserControllerGet(t *testing.T) {
 	e := echo.New()
 
 	us := mock.NewMockUserService()
-	us.Save(domain.User{Username: "restu"})
-	us.Save(domain.User{Username: "aditya"})
-	us.Save(domain.User{Username: "rachman"})
+	us.Save(domain.User{Username: "restu", Password: "rahasia"})
+	us.Save(domain.User{Username: "aditya", Password: "rahasia"})
+	us.Save(domain.User{Username: "rachman", Password: "rahasia"})
 
+	// Login First
+	loginUser := domain.User{Username: "restu", Password: "rahasia"}
+
+	loginInfo, err := json.Marshal(loginUser)
+	if err != nil {
+		t.Error("Marhalling returned user failed")
+	}
+
+	loginReq := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(loginInfo))
+	loginReq.Header.Set("Content-Type", "application/json")
+	loginRec := httptest.NewRecorder()
+	logincontext := e.NewContext(loginReq, loginRec)
+	logincontext.SetPath("/login")
+
+	uc := NewUserController(us)
+	if err := uc.Login(logincontext); err != nil {
+		t.Errorf("should not get error, get error: %s", err)
+		return
+	}
+
+	var userLogin LoginResponse
+	if err := json.Unmarshal(loginRec.Body.Bytes(), &userLogin); err != nil {
+		t.Errorf("unmarshalling returned person failed")
+	}
+
+	if userLogin.Meta.Status != 200 {
+		t.Errorf("Login fail")
+	}
+	// Get User
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", userLogin.Data.Token))
 	req.Header.Set("Content-Type", "application/jsonn")
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
-	c.SetPath("/")
+	c.SetPath("/:user_id")
+	c.SetParamNames("user_id")
+	c.SetParamValues("1")
 
-	uc := NewUserController(us)
-	uc.Get(c)
+	// uc := NewUserController(us)
+	if err := middleware.JWT([]byte(constants.SECRET_JWT))(uc.Get)(c); err != nil {
+		t.Error(err)
+	}
 
-	response := GetAllResponse{}
+	response := GetResponse{}
 
 	if err := json.Unmarshal(rec.Body.Bytes(), &response); err != nil {
-		t.Error("unmarhalling returned user failed")
+		t.Error(err)
 	}
-	users := response.Data
 
-	if len(users) != 3 {
-		t.Errorf("expecting len(users) is 3, get %d", len(users))
-	}
-	if users[0].Email != "restu@gmail.com" {
-		t.Errorf("expection users[0].Email is restu@gmail.com, get %s", users[0].Email)
-	}
-	if users[1].Email != "aditya@gmail.com" {
-		t.Errorf("expection users[0].Email is aditya@gmail.com, get %s", users[1].Email)
-	}
-	if users[2].Email != "rachman@gmail.com" {
-		t.Errorf("expection users[0].Email is rachman@gmail.com, get %s", users[2].Email)
+	user := response.Data
+	if user.Username != "restu" {
+		t.Errorf("expection username is restu, get %s", user.Username)
 	}
 }
